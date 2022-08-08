ab5e80109198e96d6e7651452e8e6936f17ec868
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-08-08 11:20:21 -0300

    instanciado o servidor mysql no projeto

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-import  express  from "express";
+import express from "express";
 import atendimentoController from "./controllers/atendimentoController.js";
+import conexao from "./infra/conexao.js";
 import bodyParser from "body-parser";
+
 const app = express()
 const port = 3000
+
 app.use(bodyParser)
 app.use(express.json());
-atendimentoController(app)
-app.listen(port, ()=>{
-    console.log(`http://localhost:${port}/`);
-})
 
+conexao.connect(erro => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log("A conexão com o banco de dados mysql foi feita com sucesso!");
+        atendimentoController(app)
+
+        app.listen(port, () => {
+            console.log(`http://localhost:${port}/`);
+        })
+
+    }
+})
